a5db91791309a9a2406318e596c4f744f3baa816
Daniel Cavalli <danfcavs@gmail.com>
2024-10-23 19:27:59 +1100

    new awesome theme gtk

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -45,10 +45,11 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
-beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+-- beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.init(gears.filesystem.get_themes_dir() .. "gtk/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "alacritty"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
